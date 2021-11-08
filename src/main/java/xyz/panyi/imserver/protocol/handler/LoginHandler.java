262c567bb7254b3ee469796d8c3eec4fb7140885
yixin.panyi <panyi@yixin.im>
2021-11-08 18:56:17 +0800

    codec message

--- src/main/java/xyz/panyi/imserver/protocol/handler/LoginHandler.java
+++ src/main/java/xyz/panyi/imserver/protocol/handler/LoginHandler.java
@@ -3,20 +3,30 @@ package xyz.panyi.imserver.protocol.handler;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import xyz.panyi.imserver.model.LoginHttpResp;
 import xyz.panyi.imserver.model.LoginReqMessage;
+import xyz.panyi.imserver.model.LoginRespMessage;
+import xyz.panyi.imserver.model.Result;
 
-@Component
+@Scope("prototype")
 @ChannelHandler.Sharable
 public class LoginHandler extends SimpleChannelInboundHandler<LoginReqMessage> {
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, LoginReqMessage msg) throws Exception {
-        if(msg.getData() == null){
-            return;
-        }
+        final LoginHttpResp req = msg.getData();
 
-        LoginHttpResp data = msg.getData();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        super.exceptionCaught(ctx, cause);
+
+        final Result result = new Result();
+        result.setResult(false);
+        result.setReason(cause.getMessage());
+        ctx.writeAndFlush(new LoginRespMessage(result));
     }
 }
